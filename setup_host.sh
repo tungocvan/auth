97d58c3934378114100fa1d55db1ba3dc5fe62b1
tungocvan <tungocvan@gmail.com>
2023-07-28 13:47:51 +0700

    env-2

--- setup_host.sh
+++ setup_host.sh
@@ -2,6 +2,7 @@ mkdir database/migrations
 composer update
 cp modules/Auth/src/Http/Controllers/AuthRouteMethods.php vendor/laravel/ui/src
 cp .env_host .env
+curl -H'Authorization: cpanel nhathhmd:U6I9SPUH8E94ONFITAKETDYAOXSAYAED' 'https://gator3199.hostgator.com:2083/execute/LangPHP/php_set_vhost_versions?version=ea-php74&vhost=laravel.tungocvan.com'
 echo "hoàn thành cài đặt"
 echo "Lưu ý setup database và cài đặt table user"
 echo "php artisan migrate:fresh"
